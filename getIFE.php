0c97c5dde6ed3af60ea59f9cd078bacfc7df99df
Geeku <ggongpei@qq.com>
2016-06-21 20:55:20 +0800

    update database

--- getIFE.php
+++ getIFE.php
@@ -9,6 +9,8 @@
 require_once 'medoo.php';
 
 /**
+ * 页面缓存,获取一次之后就保存到本地以供下次使用
+ *
  * @param $url 需要获取内容的链接
  * @param $filename 缓存的文件名
  * @return string 返回所获取的内容
@@ -17,6 +19,7 @@ function getData($url, $filename) {
     if (file_exists($filename)) {
         return file_get_contents($filename);
     } else {
+        //延时 降低获取频率
         sleep(1);
         $content = file_get_contents($url);
         file_put_contents($filename, $content);
@@ -25,6 +28,7 @@ function getData($url, $filename) {
 }
 
 /**
+ * 数据库连接
  * @return medoo
  */
 function connectDB() {
@@ -39,6 +43,8 @@ function connectDB() {
 }
 
 /**
+ * 获取所有任务描述
+ *
  * @param int $stage 任务阶段
  */
 function getTask($stage = 0) {
@@ -105,7 +111,11 @@ function getTask($stage = 0) {
     file_put_contents('./taskList', serialize($content));
 }
 
-
+/**
+ * 获取每个任务中排名前十的任务提交
+ *
+ * @param int $stage 任务阶段
+ */
 function getWork($stage = 0) {
 
     $base = 'http://ife.baidu.com/';
@@ -116,6 +126,7 @@ function getWork($stage = 0) {
 
         //连接数据库
         $db = connectDB();
+
         $i = 1;
         echo " - Stage " . $stage . " Start -\n\n";
         mkdir('./works_' . $stage . '/');
@@ -126,7 +137,6 @@ function getWork($stage = 0) {
 
             $taskID = explode('=', $item)[1];
 
-            // print_r($task);return;
             echo " - Task " . $taskID . " Start -\n";
             foreach ($task as $taskItem) {
 
@@ -135,50 +145,34 @@ function getWork($stage = 0) {
                 $data['time'] = $taskItem->submitTime;
                 $data['task'] = $taskID;
 
+                // 获取提交的任务内容
                 $workContent = getData($base . $taskItem->reviewUrl, $filename . '.' . $taskItem->groupName);
 
+                // 获取用户名
                 preg_match("/<a href=\"\/user\/profile\?userId=\d+\">(.*?)<\/a>/", $workContent, $tmp);
                 $data['user'] = $tmp[1];
 
+                // 获取review人数
                 preg_match("/已经有<em>(\d+)<\/em>人/", $workContent, $tmp);
                 $data['review'] = $tmp[1];
 
+                // 获取代码链接
                 preg_match("/<p>代码地址：<a href=\"(.*?)\"/", $workContent, $tmp);
                 $data['code_url'] = $tmp[1];
 
+                // 获取demo链接
                 preg_match("/<p>demo：<a href=\"(.*?)\"/", $workContent, $tmp);
                 $data['demo_url'] = $tmp[1];
 
+                // 获取提交的任务描述
                 preg_match("/<p class=\"submit-descr\">([\s\S]+)<\/p><\/div><ul/", $workContent, $tmp);
                 $data['description'] = $tmp[1];
 
                 $db->insert('ife_work', $data);
 
-                echo '[ ' . $i++ . ' ] Work ' . explode('=', $taskItem->reviewUrl)[1] . ' / ' . ($db->error()[2] ? $db->error()[2] : 'Successful') . "\n";
+                echo '[ ' . $i++ . ' ] Work ' . explode('=', $taskItem->reviewUrl)[1] . ' -> ' . ($db->error()[2] ? $db->error()[2] : 'Successful') . "\n";
             }
             echo " - Task " . $taskID . " End -\n\n";
-
-//            return;
-//
-//            // 任务名
-//            preg_match('/<span class="active">(.*?)<span/', $task, $tmp);
-//            $data['name'] = $tmp[1];
-//
-//            // 获取日期,截止日期和难度
-//            preg_match_all("/<div class=\"task-basic\"><p class=\"\"><span class=\"key\">发布时间：<\/span><span class=\"value\">(.*?)<\/span><\/p><p class=\"\"><span class=\"key\">截止时间：<\/span><span class=\"value\">(.*?)<\/span><\/p><p class=\"\"><span class=\"key\">难度：<\/span><span class=\"value\">(.*?)<\/span>/",  $task, $tmp);
-//            $data['date'] = @date('Y-m-d H:i:s', strtotime('2016-' . $tmp[1][0]));
-//            $data['deadline'] = @date('Y-m-d H:i:s', strtotime('2016-' . $tmp[2][0]));
-//            $data['difficulty'] = $tmp[3][0];
-//
-//            // 获取描述
-//            preg_match("/<div class=\"task-detail col-md-9\">([\s\S]+)<h3>已提交任务的团队/", $task, $tmp);
-//            $data['description'] = $tmp[1];
-//
-//            $db->insert('ife_task', $data);
-//
-//            echo '[ ' . $i++ . ' ] ' . $item . ' / ' . ($db->error()[2] ? $db->error()[2] : 'Successful') . "\n";
-//
-//            sleep(1);
         }
         echo " - Stage " . $stage . " End -\n\n";
 
@@ -186,12 +180,16 @@ function getWork($stage = 0) {
     }
 }
 
-// 获取任务
+
+
+// 获取任务描述
 //getTask(0);
 //getTask(1);
 //getTask(2);
 //getTask(3);
 
-
+// 获取任务
 //getWork(3);
-getWork(2);
+//getWork(2);
+//getWork(1);
+//getWork(0);
